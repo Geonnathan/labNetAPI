1fceab2025a14271c82fe1edfb15226fe8cf3276
Geonnathan <geonnathan1494ge@hotmail.com>
2023-04-13 23:40:10 -0600

    Base

---
+++ db_login_react.sql
@@ -0,0 +1,305 @@
+-- MySQL dump 10.13  Distrib 8.0.19, for Win64 (x86_64)
+--
+-- Host: localhost    Database: db_login_react
+-- ------------------------------------------------------
+-- Server version	5.5.5-10.3.23-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `tb_config`
+--
+
+DROP TABLE IF EXISTS `tb_config`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tb_config` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name_key` varchar(100) NOT NULL,
+  `value_key` varchar(250) NOT NULL,
+  `type` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tb_config`
+--
+
+LOCK TABLES `tb_config` WRITE;
+/*!40000 ALTER TABLE `tb_config` DISABLE KEYS */;
+INSERT INTO `tb_config` VALUES (1,'api_key','115700622','API_CREDENTIALS'),(2,'api_secret','123456','API_CREDENTIALS');
+/*!40000 ALTER TABLE `tb_config` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tb_users`
+--
+
+DROP TABLE IF EXISTS `tb_users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tb_users` (
+  `idUser` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(150) NOT NULL,
+  `email` varchar(150) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  PRIMARY KEY (`idUser`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tb_users`
+--
+
+LOCK TABLES `tb_users` WRITE;
+/*!40000 ALTER TABLE `tb_users` DISABLE KEYS */;
+INSERT INTO `tb_users` VALUES (1,'Geonnathan','Geonnathan@gmail.com','123456'),(2,'Andres','Andres@gmail.com','123456'),(3,'Carlos','Carlos@gmail.com','123456'),(5,'Andrea','Andrea@gmail.com','123456');
+/*!40000 ALTER TABLE `tb_users` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tb_webs`
+--
+
+DROP TABLE IF EXISTS `tb_webs`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tb_webs` (
+  `idWeb` int(11) NOT NULL AUTO_INCREMENT,
+  `idUser` int(11) NOT NULL,
+  `webName` varchar(150) NOT NULL,
+  `url` varchar(200) NOT NULL,
+  PRIMARY KEY (`idWeb`),
+  KEY `tb_webs_FK` (`idUser`),
+  CONSTRAINT `tb_webs_FK` FOREIGN KEY (`idUser`) REFERENCES `tb_users` (`idUser`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tb_webs`
+--
+
+LOCK TABLES `tb_webs` WRITE;
+/*!40000 ALTER TABLE `tb_webs` DISABLE KEYS */;
+INSERT INTO `tb_webs` VALUES (1,1,'Facebook','https://www.facebook.com'),(2,2,'Instagram 3','https://www.instagram.com'),(3,1,'Instagram','https://www.instagram.com'),(4,5,'Google traductor','https://translate.google.com'),(5,1,'Google','https://www.google.com'),(6,2,'Instagram 3','https://www.instagram.com');
+/*!40000 ALTER TABLE `tb_webs` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping routines for database 'db_login_react'
+--
+/*!50003 DROP FUNCTION IF EXISTS `verifyCredentials` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_unicode_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'IGNORE_SPACE,STRICT_TRANS_TABLES,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` FUNCTION `verifyCredentials`(
+   p_key VARCHAR(100),
+   p_secret VARCHAR(100)
+) RETURNS bit(1)
+BEGIN
+   
+   DECLARE v_count int;
+   SET v_count = (
+               SELECT COUNT(*) 
+                  FROM tb_config oc 
+                     WHERE oc.type = 'API_CREDENTIALS'
+                     AND (
+                           ( oc.name_key = 'api_key' and oc.value_key = p_key )
+                           OR (oc.name_key = 'api_secret' and oc.value_key = p_secret)
+                        )
+               );
+   
+   IF v_count = 2 then 
+      RETURN 1;
+   end if;
+   RETURN 0;
+
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP FUNCTION IF EXISTS `verifyUser` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_unicode_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'IGNORE_SPACE,STRICT_TRANS_TABLES,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` FUNCTION `verifyUser`(
+   p_idUser int
+) RETURNS bit(1)
+BEGIN
+   
+   DECLARE v_count int;
+   SET v_count = (
+               SELECT COUNT(*) 
+                  FROM tb_users
+                  where idUser = p_idUser
+               );
+   
+   RETURN v_count;
+
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `sp_login` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_unicode_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'IGNORE_SPACE,STRICT_TRANS_TABLES,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_login`(
+	p_key VARCHAR(100),
+    p_secret VARCHAR(100),
+    p_email VARCHAR(100),
+    p_password VARCHAR(100)
+)
+begin
+	if verifyCredentials(p_key,p_secret) = 1 then
+		select us.idUser, us.name, us.email, us.password
+		from tb_users as  us
+		where us.email = p_email and us.password = p_password;
+	end if;
+end ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `sp_selectUserWebs` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_unicode_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'IGNORE_SPACE,STRICT_TRANS_TABLES,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_selectUserWebs`(
+	p_key VARCHAR(100),
+    p_secret VARCHAR(100),
+    p_idUser int
+)
+begin
+	if verifyCredentials(p_key,p_secret) = 1 then
+		if verifyUser(p_idUser) = 1 then
+			select idWeb, idUser, webName, url from tb_webs 
+			where idUser = p_idUser;
+		end if;
+	end if;
+end ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `sp_upsertUser` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_unicode_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'IGNORE_SPACE,STRICT_TRANS_TABLES,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_upsertUser`(
+	p_key VARCHAR(100),
+    p_secret VARCHAR(100),
+    p_idUser int,
+    p_name VARCHAR(100),
+    p_email VARCHAR(100),
+    p_password VARCHAR(100)
+)
+begin
+	if verifyCredentials(p_key,p_secret) = 1 then
+		if (p_idUser = 0) then
+			insert into tb_users (name, email, password)
+			values(p_name, p_email, p_password);
+		else
+			update tb_users 
+			set name = p_name, email = p_email, password = p_password
+			where idUser = p_idUser;
+		end if;
+	end if;
+end ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `sp_upsertUserWeb` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_unicode_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'IGNORE_SPACE,STRICT_TRANS_TABLES,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_upsertUserWeb`(
+	p_key VARCHAR(100),
+    p_secret VARCHAR(100),
+    p_idWeb int(11), 
+    p_idUser int(11), 
+    p_webName varchar(150), 
+    p_url varchar(200)
+)
+begin
+	if verifyCredentials(p_key,p_secret) = 1 then
+		if (p_idWeb = 0) then
+			insert into tb_webs  (idUser, webName, url)
+			values(p_idUser, p_webName, p_url);
+		else
+			update tb_webs  
+			set webName = p_webName, url = p_url
+			where idWeb = p_idWeb and idUser = p_idUser;
+		end if;
+	end if;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-04-13 23:39:02
